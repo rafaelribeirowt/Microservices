7d479373e4b5d6b04d1c066acd385f373514ff4c
Rafael Ribeiro <rafael.ribeirowt@gmail.com>
2022-11-03 16:51:49 -0300

    UserFeignClient

--- hr-user/src/main/java/com/raeltecnologia/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/raeltecnologia/hruser/HrUserApplication.java
@@ -1,27 +1,19 @@
 package com.raeltecnologia.hruser;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication implements CommandLineRunner {
+public class HrUserApplication  {
 	
-	@Autowired
-	private BCryptPasswordEncoder passwordEncode;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		//System.out.println("Bacrypt = " + passwordEncode.encode("123456"));
-		
-	}
+	
 
 }
